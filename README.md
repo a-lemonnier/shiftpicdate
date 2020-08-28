1d48c1a27f909f73b6ed58b98700c4798d864066
a-lemonnier <audric.lemonnier@obspm.fr>
2020-08-28 19:29:56 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # shiftpicdate
-Shift the exif tag date.
+
+Shift the exif tag date using Exiv2 C++ library.
+
+> Dependencies:
+- C++ 17
+- Boost
+- Exiv2
+
+
+```
+Usage:
+  -h [ --help ]            Display this help message.
+  -q [ --quiet ]           toggle verbosity off.
+  -f [ --filename ] arg    Specify one file.
+  -D [ --directory ] arg   Directory where the pictures are.
+  -t [ --thread ] arg (=4) Number of threads.
+                           
+  -S [ --second ] arg (=0)  
+  -M [ --minute ] arg (=0)  
+  -H [ --hour ] arg (=0)    
+  -d [ --day ] arg (=0)     
+  -m [ --month ] arg (=0)  Based on 30 days/month.
+  -y [ --year ] arg (=0)   Based on 365 days/year.
+  --DST                    Enable DST.
+
+```
--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <filesystem>
+#include <cmath>
 
 #include <vector>
-#include <list>
 
 #include <algorithm>
 
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <locale>
 
 #include <chrono>
 
 #include <thread>
 #include <future>
 
-#include <cmath>
+// ----------------------------------------------------
 #include <exiv2/exiv2.hpp>
 
 #include <boost/program_options.hpp>
@@ -40,36 +41,39 @@ namespace fs = boost::filesystem;
 #else
 #error "No filesystem header found"
 #endif
+// ----------------------------------------------------
 
-#include "boost/date_time/gregorian/gregorian.hpp"
-#include "boost/date_time/posix_time/posix_time.hpp"
-
+// ----------------------------------------------------
 #define EXTENSION_LIST {".JPEG", ".jpeg", \
                         ".JPG",  ".jpg",  \
                         ".CR2",  ".cr2"  }
 
-bool bQuiet;                        
+bool bQuiet;
+bool bIsDST;
 static const std::string dateTimeFormat{ "%Y:%m:%d %H:%M:%S" };
+// ----------------------------------------------------
 
+// Proto ----------------------------------------------
 std::string stoyear(long long t);
 inline void shift(std::vector<std::string> vList, long long Diff);
+inline void shift_q(std::vector<std::string> vList, long long Diff);
 inline bool test_ext(const std::string &sS);
-
+// ----------------------------------------------------
 
 int main(int argc, char **argv) {
     
-    #ifdef HAS_BOOST_TIMER    
-    boost::timer::cpu_timer btTimer;
-    #endif    
+#ifdef HAS_BOOST_TIMER    
+boost::timer::cpu_timer btTimer;
+#endif    
     
-    // Parse cmd line
-    // ----------------------------------------------------  
+// Parse cmd line
+// ----------------------------------------------------  
     namespace po = boost::program_options;
     po::options_description description("Usage");
     
     description.add_options()
     ("help,h"                                                   , "Display this help message.")
-    ("quiet,q"                                                  , "toggle off verbosity.")
+    ("quiet,q"                                                  , "toggle verbosity off.")
     ("filename,f",   po::value<std::string>()                   , "Specify one file.")
     ("directory,D",  po::value<std::string>()                   , "Directory where the pictures are.")
     ("thread,t",     po::value<unsigned int>()->default_value(4), "Number of threads.\n")
@@ -78,7 +82,8 @@ int main(int argc, char **argv) {
     ("hour,H",       po::value<long long>()->default_value(0)   , " ")
     ("day,d",        po::value<long long>()->default_value(0)   , " ")
     ("month,m",      po::value<long long>()->default_value(0)   , "Based on 30 days/month.")
-    ("year,y",       po::value<long long>()->default_value(0)   , "Based on 365 days/year.");
+    ("year,y",       po::value<long long>()->default_value(0)   , "Based on 365 days/year.")
+    ("DST"                                                      , "Enable DST.");
     
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
@@ -93,15 +98,16 @@ int main(int argc, char **argv) {
     // ----------------------------------------------------
     
     bQuiet=!vm.count("quiet");
+    bIsDST=vm.count("DST");
     
     // ---------------------------------------------------- 
     
     const long long Diff=vm["second"].as<long long>()
-    +vm["minute"].as<long long>()*60
-    +vm["hour"].as<long long>()*3600
-    +vm["day"].as<long long>()*3600*24
-    +vm["month"].as<long long>()*3600*24*30
-    +vm["year"].as<long long>()*3600*24*365;
+                        +vm["minute"].as<long long>()*60
+                        +vm["hour"].as<long long>()*3600
+                        +vm["day"].as<long long>()*3600*24
+                        +vm["month"].as<long long>()*3600*24*30
+                        +vm["year"].as<long long>()*3600*24*365;
     
     
     // ---------------------------------------------------- 
@@ -124,23 +130,33 @@ int main(int argc, char **argv) {
             
             std::vector<std::vector<std::string> > vvsList_divided;
             
-            for(int i=0; i< max_thread-1; i++) 
-                vvsList_divided.emplace_back(std::vector<std::string>(vList.begin()+i*size_divided, vList.begin() + size_divided*(i+1)));
+            if (static_cast<int>(vList.size())>max_thread) {
+                for(int i=0; i< max_thread-1; i++) 
+                    vvsList_divided.emplace_back(std::vector<std::string>(vList.begin()+i*size_divided, vList.begin() + size_divided*(i+1)));
+                
+                // 1 extra thread for extra files : rest of division
+                vvsList_divided.emplace_back(std::vector<std::string>(vList.begin()+(max_thread-1)*size_divided, vList.end()));
+            }
+            else 
+                    vvsList_divided.emplace_back(vList);
             
-            // 1 extra thread for extra files : rest of division
-            vvsList_divided.emplace_back(std::vector<std::string>(vList.begin()+(max_thread-1)*size_divided, vList.end()));
             
             std::launch lFlag=std::launch::async;
             std::vector<std::future<void> > vfThread;
             
-            for(auto t_list: vvsList_divided) 
-                vfThread.emplace_back(std::async(lFlag, shift, t_list, Diff));
-            
+            if (bQuiet)
+                for(auto t_list: vvsList_divided) 
+                    vfThread.emplace_back(std::async(lFlag, shift, t_list, Diff));
+            else
+                for(auto t_list: vvsList_divided) 
+                    vfThread.emplace_back(std::async(lFlag, shift_q, t_list, Diff));
+                
             std::cout << "- Number of files: " << vList.size() << ".\n";
             std::cout << "- Number of threads: " << max_thread << ".\n";
             std::cout << "- Shift date by: " << Diff << "s -> " << stoyear(Diff) << ".\n";
             
-            std::cout << "- Start " << max_thread << " threads.\n";
+            if (static_cast<int>(vList.size())>max_thread)
+                std::cout << "- Start " << max_thread << " threads.\n";
             std::for_each(vfThread.begin(), vfThread.end(), [](std::future<void> &th) { th.get(); });
         }
         else
@@ -157,14 +173,16 @@ int main(int argc, char **argv) {
             std::cerr << "File does not exist.\n";
     }
     
-    #ifdef HAS_BOOST_TIMER
-    std::cout << "\n" << btTimer.format();
-    #endif
+#ifdef HAS_BOOST_TIMER
+std::cout << "\n" << btTimer.format();
+#endif
     
     return EXIT_SUCCESS;
 }
 
-
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
 
 std::string stoyear(long long t) {
     std::stringstream ssS;
@@ -198,34 +216,70 @@ void shift(std::vector<std::string> vList, long long Diff) {
     for(const auto &sFile: vList)
         if (test_ext(sFile)) {
             
-            if (fs::exists(sFile)) {
-                if (bQuiet)
-                    std::cout << "-> " << sFile << "\n";
+                std::cout << "-> " << sFile << "\n";
                 
                 auto pImg=Exiv2::ImageFactory::open(sFile);
-                
-                //                 pImg.get();
+
                 pImg->readMetadata();
                 
                 Exiv2::ExifData &exifData = pImg->exifData();
                 
-                std::string sDatetime=exifData["Exif.Image.DateTime"].toString();
+                std::stringstream ssS(exifData["Exif.Image.DateTime"].toString());
                 
-                std::tm dt={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                std::tm dt={ };
+                if (bIsDST) dt.tm_isdst=bIsDST;
+                
+                ssS >> std::get_time(&dt, dateTimeFormat.c_str());
                 
                 std::time_t iEpoch=std::mktime(&dt);
-                
+                                
                 iEpoch+=Diff;
                 
-                std::stringstream ssS;
+                ssS.clear();
                 ssS << std::put_time(std::localtime(&iEpoch), (dateTimeFormat).c_str());
-                
+                        
                 exifData["Exif.Image.DateTime"]=ssS.str();
-                
+                    
                 pImg->setExifData(exifData);
                 pImg->writeMetadata();
-//                 pImg->readMetadata();
+                pImg->readMetadata();
+            }
+            else
+                std::cerr << "/!\\ File does not exists.\n";
+}
+
+void shift_q(std::vector<std::string> vList, long long Diff) {
+    
+    for(const auto &sFile: vList)
+        if (test_ext(sFile)) {
+            
+            if (fs::exists(sFile)) {
                 
+                auto pImg=Exiv2::ImageFactory::open(sFile);
+                
+                   pImg->readMetadata();
+                
+                Exiv2::ExifData &exifData = pImg->exifData();
+                
+                std::stringstream ssS(exifData["Exif.Image.DateTime"].toString());
+                std::tm dt={ };
+                
+                if (bIsDST) dt.tm_isdst=bIsDST;
+                
+                ssS >> std::get_time(&dt, dateTimeFormat.c_str());
+                
+                std::time_t iEpoch=std::mktime(&dt);
+                                
+                iEpoch+=Diff;
+                
+                ssS.clear();
+                ssS << std::put_time(std::localtime(&iEpoch), (dateTimeFormat).c_str());
+                        
+                exifData["Exif.Image.DateTime"]=ssS.str();
+                    
+                pImg->setExifData(exifData);
+                pImg->writeMetadata();
+                pImg->readMetadata();
             }
             else
                 std::cerr << "/!\\ File does not exists.\n";
